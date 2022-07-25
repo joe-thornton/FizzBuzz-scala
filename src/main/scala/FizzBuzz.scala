5dbc3c650018b82bc07f22024f3602945ef16425
Joe Thornton <thornton.jp@gmail.com>
2022-07-25 18:30:29 +0100

    refactored to remove redundant initial if statements no longer needed with array mapping

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,10 +1,5 @@
 object FizzBuzz {
   def generate(number: Int): String = {
-    if (number == 1) {
-      "1"
-    } else if (number == 2) {
-      "1, 2"
-    } else {
       val intArray = Array.range(1, number+1)
       val stringArray = intArray.map { x =>
         if (x % 3 == 0) {
@@ -12,6 +7,5 @@ object FizzBuzz {
         } else x
       }
       stringArray.mkString(", ")
-    }
   }
 }
