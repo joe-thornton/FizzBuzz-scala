b05d5193fc07a2a414e004967bfe21672c18f882
Joe Thornton <thornton.jp@gmail.com>
2022-07-25 18:39:12 +0100

    tests passing for all cases

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,14 +1,12 @@
 object FizzBuzz {
   def generate(number: Int): String = {
       val intArray = Array.range(1, number+1)
-      val stringArray = intArray.map { x =>
-        if (x % 3 == 0) {
-          "Fizz"
-        } else if (x % 5 == 0) {
-          "Buzz"
-        }
+      val fizzBuzzArray = intArray.map { x =>
+        if (x % 3 == 0 && x % 5 == 0) "FizzBuzz"
+        else if (x % 3 == 0) "Fizz"
+        else if (x % 5 == 0) "Buzz"
         else x
       }
-      stringArray.mkString(", ")
+      fizzBuzzArray.mkString(", ")
   }
 }
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -19,4 +19,9 @@ class FizzBuzzTest extends AnyFunSuite {
   test("lists the numbers up to five") {
     assert(FizzBuzz.generate(5) === "1, 2, Fizz, 4, Buzz")
   }
+
+  test("works for numbers divisible by three and five") {
+    assert(FizzBuzz.generate(15) === "1, 2, Fizz, 4 Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz")
+  }
+
 }
